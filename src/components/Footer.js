b9b4bcfcdbda2c51b14112cb2354ef28e00fec05
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-07-25 12:32:46 -0700

    adding initial files

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                <p>
+            <a href ="https://github.com/ns8279" target = "_blank" rel = "noopener noreferrer" />
+            Github
+            </p>
+            
+            </div>
+            <a href ="https://github.com/ns8279" target = "_blank" rel = "noopener noreferrer" />
+            
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import NavBars from './NavBar';
+import About from './pages/About';
+//import Project from './Projects';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Portfolio() {
+  const [currentPage, handlePageChange] = useState('About');
+
+  const renderPage = () => {   
+        switch(currentPage) 
+        {
+  
+          case "About":   return <About />;
+          //case "Project":   return <Project />;
+          case "Contact": return <Contact />;
+          case "Resume": return <Resume />;
+
+          default: return <About />
+          
+        }
+      
+  
+      
+  };
+  
+
+  return (
+    <div>
+      <NavBars currentPage={currentPage} handlePageChange={handlePageChange} />
+      <div>
+        {
+          renderPage()
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
