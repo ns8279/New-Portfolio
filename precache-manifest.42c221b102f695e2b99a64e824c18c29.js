4554045a54a2355b5b8464ee79d6ddf04c3c69c7
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-07-26 11:32:38 -0700

    Updates

---
+++ precache-manifest.42c221b102f695e2b99a64e824c18c29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1561e9f956b9e90ac14aa311fb2105e6",
+    "url": "/New-Portfolio/index.html"
+  },
+  {
+    "revision": "4c2bf55f5fcbaa8f0d33",
+    "url": "/New-Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "478fa1bcf44ada53785e",
+    "url": "/New-Portfolio/static/css/main.a748f84f.chunk.css"
+  },
+  {
+    "revision": "4c2bf55f5fcbaa8f0d33",
+    "url": "/New-Portfolio/static/js/2.ed170f0e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/New-Portfolio/static/js/2.ed170f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478fa1bcf44ada53785e",
+    "url": "/New-Portfolio/static/js/main.483427fa.chunk.js"
+  },
+  {
+    "revision": "b4d4e5625fd5ec801882",
+    "url": "/New-Portfolio/static/js/runtime-main.54b38be3.js"
+  },
+  {
+    "revision": "6a4cf4853c511fba1f5b33b38e31f7cd",
+    "url": "/New-Portfolio/static/media/cover-img.6a4cf485.jpg"
+  }
+]);
